fbd998b8dee4a2a73d7c39c31e0d11284b487aa3
Sam Philip <seivadmas@gmail.com>
2014-03-06 16:53:28 -0300

    Iteration C complete, implemented caching

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4 # sidebar contains 4 links
+    assert_select '#main .entry', 4 # four product entries defined by test fixture
+    assert_select 'h3', 'Programming Ruby 1.9' # one of the products has this title
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
