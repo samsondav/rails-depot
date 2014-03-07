e948c852cca4fd628876188f2be49e2a77888901
Sam Philip <seivadmas@gmail.com>
2014-03-07 17:01:34 -0300

    Implemented iteration D3, add to cart button

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,8 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+  
   # GET /line_items
   # GET /line_items.json
   def index
@@ -24,11 +26,13 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+#    @line_item = @cart.line_items.build(product: product)
+    @line_item = product.line_items.build(cart: @cart) #does this work as an alternative? - YES IT DOES
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
