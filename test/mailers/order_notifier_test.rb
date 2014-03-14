6fff5d5bba8d3fa7deb31794fd83607dcfdefc46
Sam Philip <seivadmas@gmail.com>
2014-03-14 13:38:23 -0300

    Iteration G1 complete, email and tests added

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Sam's Amazing Online Store Order Confirmation", mail.subject
+    assert_equal ["email1@example.com"], mail.to
+    assert_equal ["stavros.foreman@gmail.com"], mail.from
+#    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Sam's Amazing Online Store Order Shipped", mail.subject
+    assert_equal ["email1@example.com"], mail.to
+    assert_equal ["stavros.foreman@gmail.com"], mail.from
+#    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
   end
 
 end
