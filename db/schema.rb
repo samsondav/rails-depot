af3a7fb4238b3b17be495032a89cddc438fdcd2e
Sam Philip <seivadmas@gmail.com>
2014-03-15 17:55:56 -0300

    Add new model User and respective tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314191321) do
+ActiveRecord::Schema.define(version: 20140315010336) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 20140314191321) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
