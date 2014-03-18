9062d717bb5bee5de94c078fd5a568c4a4c64077
Sam Philip <seivadmas@gmail.com>
2014-03-18 19:41:05 -0300

    Implemented automatic currency conversion and display in euros in ES locale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
+  
+  def local_currency(price)
+    euro_rate = 0.72
+    if I18n.locale.to_s == 'es'
+      price * euro_rate
+    else
+      price
+    end
+  end
 end
