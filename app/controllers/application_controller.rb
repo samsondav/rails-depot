f1366cf01bbab247c0cd345e3353d5eea709c8f3
Sam Philip <seivadmas@gmail.com>
2014-03-17 21:09:52 -0300

    Iteration J1 complete, added preliminary I18n support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authorize
+  before_action :set_i18n_locale_from_params
   
   protected
   def authorize
@@ -21,5 +22,20 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, notice: 'Please log in'
       end
     end
+  end
+  
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+  
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,64 +10,17 @@ Depot::Application.routes.draw do
 
   resources :users
 
-  resources :orders
-  
-  resources :line_items
-
-  resources :carts
-
   get "store/index"
-  resources :products do #draws out automatic plural routes for products controller
+  resources :products do
     get :who_bought, on: :member
   end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store' #TODO: try commenting out the as: store and see if it affects path_to store_path. HINT: it will.
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
+  
+  scope '(:locale)' do
+    resources :orders
     resources :line_items do
-      member do
-        post 'decrement'
-      end
+      post :decrement, on: :member
     end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 end
